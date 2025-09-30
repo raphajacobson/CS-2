f2e6a7d7795e446fbee61f4063b95be29d73f36e
raphajacobson <rjacobson28@gcds.net>
2025-09-30 13:59:05 -0400

    Update what_is_in_a_name.py

--- what_is_in_a_name.py
+++ what_is_in_a_name.py
@@ -1,6 +1,10 @@
+#Name: Rapha Jacobson
+#Description: Asks user for their name and gives them a list of multiple ways they can manipulate their name
+#Bonuses: Return boolean if name contains a title/distinction, remove_title function, Build a menu, Return name as sorted array of characters
+#Bugs: Code does not work for special characters, code does not account for some titles which I did not define in the "titles" list
+#1.0 LRJ
 import random                                                                                                   #import random library
 
-user_name = input("What is your name?")                                                                         #ask user what their name is
 
 def split_name(user_name):
     parts = []                                                                                                  #define variable "parts" as an empty list
@@ -26,7 +30,6 @@ def reverse_and_display(user_name):                                             
     Returns:
         print: The name backwards
     '''
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
     return(user_name[::-1])                                                                                     #return to the function the user's name backwards
 
 def count_vowels(user_name):                                                                                    #define count vowels function with user_name as an argument
@@ -37,7 +40,6 @@ def count_vowels(user_name):                                                    
     Returns:
         print: The number of vowels in the name
     '''
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
     vowel_list = ["a", "e", "i", "o", "u"]                                                                      #create a list of vowels
     count = 0                                                                                                   #set vowel count to 0
 
@@ -54,7 +56,6 @@ def consonant_frequency(user_name):                                             
     Returns:
         print: number of consonants in name
         '''
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
     consonant_list = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"] #list of all consonants
     consonant_count = 0                                                                                         #set consonant count to 0
 
@@ -97,7 +98,7 @@ def middle_name(user_name):                                                     
         if char == " ":
             space += 1
     if space < 2:
-        middle = "no middle name"
+        middle = ("no middle name")
     else:
         lastspace = len(user_name)-1                                                                            #define variable "lastspace" as the length of the user's name
         for i in range(len(user_name)-1, -1, -1):                                                               #for loop (for every character in the length of the user's name going from the last character to first)
@@ -124,15 +125,23 @@ def last_name(user_name):                                                       
     Returns:
         the user's last name
     '''
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
-    finalspace = len(user_name)-1                                                                               #define the variable "finalspace" as the length of the user's name starting from the last character and ending at the first character
-    for i in range(len(user_name)-1, -1, -1):                                                                   #for loop (for every character in the length of the user's name going backwards)
-        if user_name[i] == " ":                                                                                 #if a character in the user's name is a space
-            finalspace = finalspace +1                                                                          #add one to the finalspace counter
-            break                                                                                               #end for loop
-        else:                                                                                                   #otherwise
-            finalspace = finalspace -1                                                                          #subtract one from the finalspace counter
-    return user_name[finalspace :]                                                                              #return the user's name from the last space to the end of their name (aka their last name)
+    space = 0
+    for char in user_name:
+        if char == " ":
+            space += 1
+    if space < 1:
+        last = "no last name"
+    else:
+        user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
+        finalspace = len(user_name)-1                                                                               #define the variable "finalspace" as the length of the user's name starting from the last character and ending at the first character
+        for i in range(len(user_name)-1, -1, -1):                                                                   #for loop (for every character in the length of the user's name going backwards)
+            if user_name[i] == " ":                                                                                 #if a character in the user's name is a space
+                finalspace = finalspace +1                                                                          #add one to the finalspace counter
+                break                                                                                               #end for loop
+            else:                                                                                                   #otherwise
+                finalspace = finalspace -1                                                                          #subtract one from the finalspace counter
+            last = user_name[finalspace :] 
+    return last                                                                                              #return the user's name from the last space to the end of their name (aka their last name)
 
 def hyphen(user_name):                                                                                          #define the hyphen function with user_name as an argument
     '''
@@ -156,7 +165,6 @@ def convert_upper(user_name):                                                   
     Returns:
         the user's name in uppercase
     '''                                                                                  
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
     output = ""                                                                                                 #define the variable "output" as an empty string
     for char in user_name:                                                                                      #for loop (for every character in the user's name)
         num = ord(char)                                                                                         #create variable "num" defined as the ordinal variable for every character in the user's name
@@ -176,7 +184,6 @@ def convert_lower(user_name):                                                   
     Returns:
         the user's name in all lowercase
     '''                                                                                  
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
     output = ""                                                                                                 #define the variable "output" as an empty string
     for char in user_name:                                                                                      #for loop (for every character in the user's name)
         num = ord(char)                                                                                         #create variable "num" defined as the ordinal variable for every character in the user's name                                                                                                                  
@@ -197,7 +204,6 @@ def scramble_name(user_name):                                                   
     Returns:
         the user's name randomly scrambled
     '''
-    user_name = remove_title(user_name)                                                                         #redefine user_name as the user's name without a title (useing remove_title function)
     char_list = list(user_name)                                                                                 #create the "char_list" variable and define it as all of the characters in the user's name converted to a list
     random.shuffle(char_list)                                                                                   #randomly shuffle the character list
     new_word = ""                                                                                               #create the variable "new_word" and define it as an empty string
@@ -220,7 +226,7 @@ def palindrome(user_name):                                                      
         return False                                                                                            #return a false boolean value
 
 
-def get_initials(first, middle, last):
+def get_initials(user_name):
     '''
     Finds initials of user
     Args:
@@ -230,24 +236,12 @@ def get_initials(first, middle, last):
     Returns:
         The user's initials
     '''
-    user_name = remove_title(user_name)
-    space = 0                                                                                                   #set variable "space" to 0
-    for char in user_name:                                                                                      #for loop (for every character in user's name)
-        if char == " ":                                                                                         #if a character in the user's name is a space
-            space += 1                                                                                          #add one to the space value
-    firsti = list(first)                                                                                        #creates variable "firsti" which makes the user's first name into a list
-    lasti = list(last)                                                                                          #creates variable "lasti" which makes the user's last name into a list
-    middlei = list(middle)                                                                                      #creates variable "middlei" which makes the user's middle name into a list
-    if space == 0:                                                                                              #if the space count equals 0
-        initials = firsti[0]                                                                                    #variable "initials" equals the first character in the user's first name
-    elif space == 1:                                                                                            #if the space count equals 1
-        initials = firsti[0], lasti[0]                                                                          #variable "initials" equals the first character in the user's first name and the first character in the user's last name
-    elif space == 2:                                                                                            #if the space count equals 2
-        initials = firsti[0], middlei[0], lasti[0]                                                              #variable "initials" equals the first character in the user's first name, middle name, and last name
-    else:                                                                                                       #otherwise
-        initials = firsti[0], middlei [0], lasti[0]                                                             #variable "initials" equals the first character in the user's first name, middle name, and last name
-
-    return "".join(initials)                                                                                    #return the variable "initials" joined together
+    parts = split_name(user_name)
+    initials = []
+    for item in parts:
+        initials.append(item[0])
+    return "".join(convert_upper(initials))
+    
 
 def remove_title(user_name):
     '''
@@ -257,15 +251,15 @@ def remove_title(user_name):
     Returns:
         Name without title
     '''
-    titles = ["Dr.", "Sir", "Ph.d", "Esq", "Mr.", "Mrs.", "Ms."]                                                #define variable "titles" as a list with all possible titles
+    titles = ["Dr.", "dr.", "dr", "sir", "Sir ", "Jr.", "jr", "Ph.d", "phd", "ph.d", "esq", "mr.", "mr", "mrs", "mrs.", "ms.", "ms" "Esq ", "Mr. ", "Mrs. ", "Ms. "]                                         #define variable "titles" as a list with all possible titles
     
-    parts = list(split_name(user_name))                                                                         #create variable "parts" as the user's name split into a list at every space
-    first_part = parts[0]                                                                                       #create variable "first_part" and define it as the first index of the "parts" list (the first word of the user's name)                  
+    parts = split_name(user_name)                                                                               #create variable "parts" as the user's name split into a list at every space                                                                                      #create variable "first_part" and define it as the first index of the "parts" list (the first word of the user's name)                  
     
-    if first_part in titles:                                                                                    #if the user's first_word is in the "titles" list
-        parts.remove(first_part)                                                                                #remove the title from the parts list
-        user_name = parts                                                                                       #redefine user_name as the user's name without its title
-    return " ".join(user_name)                                                                                          #return the user's name without a title
+    for title in titles:
+        if title in parts:
+            parts.remove(title)                                                                                #remove the title from the parts list
+    user_name = " ".join(parts)
+    return user_name                                                                                     #return the user's name without a title
 
 def find_title(user_name):
     '''
@@ -275,7 +269,7 @@ def find_title(user_name):
     Returns:
         Boolean value if there is a title or not
     '''
-    titles = ["Dr. ", "Sir ", "Ph.d ", "Esq ", "Mr. ", "Mrs. ", "Ms. "]                                             #define variable "titles" as a list with all possible titles
+    titles = ["Dr. ", "dr.", "dr" "sir", "Sir ", "Jr.", "jr", "Ph.d", "phd", "ph.d", "esq", "mr.", "mr", "mrs", "mrs.", "ms.", "ms" "Esq ", "Mr. ", "Mrs. ", "Ms. "]                                             #define variable "titles" as a list with all possible titles
 
     firstspace = 0                                                                                                  #create variable "firstspace" and set it to 0
     for i in range(0,len(user_name)):                                                                               #for loop (for every character in the length of the user's name)    
@@ -298,7 +292,6 @@ def sort_name(user_name):                                                       
     Returns:
         name with all characters in alphabetical order
     '''
-    user_name = remove_title(user_name)                                                                             #redefine user_name as the user's name without a title (useing remove_title function)
     new_list = []                                                                                                   #create variable "new_list" and define it as an empty list
     for i in user_name:                                                                                             #for every character in the user's name
         new_list.append(i)                                                                                          #add every character to new_list
@@ -309,9 +302,13 @@ def sort_name(user_name):                                                       
 
 
 def main():                                                                                                         #define main function (where every function is called and executed)
+    user_name = input("What is your name?")                                                                         #ask user what their name is
     while True:
         choice = input("what would you like to do with your name? (insert number) \n 0. remove title from name \n 1. print name backwards \n 2. count vowels \n 3. count consonants \n 4. return first name \n 5. return last name \n 6. return middle name(s) \n 7. identify if there is a hyphen in your name \n 8. make name lowercase \n 9. make name uppercase \n 10. scramble letters in name \n 11. identify if name is a palindrome \n 12. Get initials of user's name \n 13. identify if the name includes a title \n 14. split name into list \n 15. sort name's characters alphabetically \n :")
-        if choice == "1":
+        if choice == "0":
+            user_name = remove_title(user_name)
+            print(f"Your user_name from now on is: {user_name}")
+        elif choice == "1":
             print(reverse_and_display(user_name))
         elif choice == "2":
             print(count_vowels(user_name))
@@ -334,9 +331,7 @@ def main():                                                                     
         elif choice == "11":
             print(palindrome(user_name))
         elif choice == "12":
-            print(get_initials(first_name(user_name), middle_name(user_name), last_name(user_name)))
-        elif choice == "0":
-            print(remove_title(user_name))
+            print(get_initials(user_name))
         elif choice == "13":
             print(find_title(user_name))
         elif choice == "14":
