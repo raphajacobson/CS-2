d78c7dacaa1cbc1f4c45573e48f49f12e1a94024
raphajacobson <rjacobson28@gcds.net>
2025-10-23 11:39:55 -0400

    Update rapha_jacobson_post_office.py

--- rapha_jacobson_post_office.py
+++ rapha_jacobson_post_office.py
@@ -1,4 +1,15 @@
-import time
+'''
+rapha_jacobson_post_office.py
+
+Description: Post office program that calculates the price it will cost the user to ship any given 
+package across any given zip code. 
+
+Features: calculates price, calculates the distance. 
+
+Log: 1.0
+
+Bugs: Bugs go here
+'''
 
 def determine_class(length, height, thickness):
 
@@ -40,23 +51,17 @@ def cost_association(package_class, zip_distance):
     cost = 0
     zip_distance
     if package_class == "regular post card":
-        cost += .20
-        cost += .03 * zip_distance
+        cost = .20 + (.03*zip_distance)
     elif package_class == "large post card":
-        cost += .37
-        cost += .03 * zip_distance
+        cost = .37 + (.03*zip_distance)
     elif package_class == "envelope":
-        cost += .37
-        cost += .04 * zip_distance
+        cost = .37 + (.04*zip_distance)
     elif package_class == "large envelope":
-        cost += .60
-        cost +=.05 * zip_distance
+        cost = .6 + (.05*zip_distance)
     elif package_class == "package":
-        cost += 2.95
-        cost += .25 * zip_distance
+        cost = 2.95 + (.25*zip_distance)
     elif package_class == "large package":
-        cost += 3.95
-        cost += .35 * zip_distance
+        cost = 3.95 + (.35*zip_distance)
 
     return cost
 
@@ -68,55 +73,37 @@ def main():
     while True:
         data = input("what are the specs of your package?:")
         data_list = data.split(",")
-        length = float(data_list[0])
-        height = float(data_list[1])
-        thickness = float(data_list[2])
-        zip_start = int(data_list[3])
-        zip_end = int(data_list[4])
-        
-        if length + height + height + thickness + thickness > 130: 
-            time.sleep(2)
-            print("Package is unmailable because it is too large")
-        elif length < 3.5 or height < 3.5 or thickness < .007:
-            time.sleep(2)
-            print("Package is unmailable because it is too small")
-        elif zip_start < 1 or zip_start > 99999:
-            print ("Your starting zip code was invalid") 
-        elif zip_end < 1 or zip_end > 99999:
-            time.sleep(2)
-            print("Your end zip code is invalid")
+        if len(data_list) != 5:
+            print("Data must be 5 numeric values, comma separated.")
         else:
-            print("We will calculate the price of your package")
-            time.sleep(2)
-            break 
+            length = float(data_list[0])
+            height = float(data_list[1])
+            thickness = float(data_list[2])
+            zip_start = int(data_list[3])
+            zip_end = int(data_list[4])
+            
+        
+            if length + height + height + thickness + thickness > 130: 
+                print("Package is unmailable because it is too large")
+            elif length < 3.5 or height < 3.5 or thickness < .007:
+                print("Package is unmailable because it is too small")
+            elif zip_start < 1 or zip_start > 99999:
+                print ("Your starting zip code was invalid") 
+            elif zip_end < 1 or zip_end > 99999:
+                print("Your end zip code is invalid")
+            else:
+                print("We will calculate the price of your package")
+                
     
+            package_class = determine_class(length, height, thickness)
+            start = zip_distance(zip_start)
+            end = zip_distance(zip_end)
+            distance = abs(zip_distance(zip_start) - zip_distance(zip_end))
+            
 
-    start = zip_distance(zip_start)
-    end = zip_distance(zip_end)
-    distance = abs(zip_distance(zip_start) - zip_distance(zip_end))
-    print(type(zip_distance))
-    #cost_association(package_class, zip_distance)
+            price = cost_association(package_class, distance)
+            print(f'{price:.2f}'.lstrip('0'))
 
         
 
 main()
-
-"""
-1. make sure input is valid
-    - Valid length, height, width, and zip codes
-2. determine classes
-    -determine specifics within class (or if mailiable at all)
-        - Length
-        - Height
-        - Width
-3. Zip code distance
-    - Find difference between start and end zipcode
-4. Cost association
-    - using the class and zip code distance
-5. Define the total cost
-    - class cost + distance cost = total cost
-6. Main
-    - input package specs
-    validate input()
-    print(total_cost())
-"""
