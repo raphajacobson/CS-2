137bb3d77740d98ba713331ace51db0013dae913
raphajacobson <rjacobson28@gcds.net>
2025-12-09 13:41:42 -0500

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -84,13 +84,56 @@ def age_analysis(file):
                     continue
         all_surv_ages = sum(surv_age)
         all_died_ages = sum(died_age)
+        oldest_pass = max(age_list)
+        youngest_pass = min(age_list)
         avg_surv_age = all_surv_ages/(len(surv_age))
         avg_died_age = all_died_ages/(len(died_age))
         output.write(f"The average age of all survivors of the titanic is: {avg_surv_age}\n")
-        output.write(f"The average age of all the non-survivors of the titanic is: {avg_died_age}")
+        output.write(f"The average age of all the non-survivors of the titanic is: {avg_died_age}\n")
+        output.write(f"The youngest passenger is {youngest_pass} years old\n")
+        output.write(f"The oldest passenger is {oldest_pass} years old\n")
       except FileExistsError:
         print(f"Error {'ageanalysis'} not found")
 
+def class_based_analysis(file):
+   
+  try: 
+    with open('classbasedanalysis.csv', 'w') as output:
+      next(file)
+      firstc_surv = 0
+      secondc_surv = 0
+      thirdc_surv = 0
+      fare_fc = []
+      fare_sc = []
+      fare_tc = []
+      c1 = 0
+      c2 = 0
+      c3 = 0
+      for line in file:
+        data = line.strip().split(',')
+        if data[2] == "1":
+          c1 += 1
+          if data[1] == "1":
+            firstc_surv += 1
+          fare_fc.append(data[10])
+        elif data[2] == "2":
+          c2 += 2
+          if data[1] == "1":
+            secondc_surv += 1
+          fare_sc.append(data[10])
+        elif data[2] == "3":
+          c3 += 3
+          if data[1] == "1":
+            thirdc_surv += 1
+          fare_tc.append(data[10])
+      first_sr = round((firstc_surv/c1)*100)
+      second_sr = round((secondc_surv/c2)*100)
+      third_sr = round((thirdc_surv/c3)*100)
+    output.write(f"The average survival rate of the first class is {first_sr}%\n")
+    output.write(f"The average survival rate of the second class is {second_sr}%\n")
+    output.write(f"The average survival rate of the third class is {third_sr}%\n")
+  except FileExistsError:
+    print(f"Error {'classbasedanalysis.csv'} not found")
 
 
 
@@ -110,6 +153,7 @@ def main():
       file.seek(1)
       age_analysis(file)
       file.seek(1)
+      class_based_analysis(file)
 
 
   except FileNotFoundError:
@@ -117,4 +161,3 @@ def main():
   
 
 main()
-
